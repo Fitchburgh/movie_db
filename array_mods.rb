657dfb3aa626f5e629e680ebde655269d728835d
Brandon Fitch <fitchbj@gmail.com>
2016-09-23 14:10:01 -0400

    Able to create an array of arrays for movie_id, movie_rating.

--- array_mods.rb
+++ array_mods.rb
@@ -1,5 +1,17 @@
+# class Array
+#   def get_odd_indexes
+#     values_at(* each_index.select(&:odd?))
+#   end
+# end
+
+
 class Array
-  def get_odd_indexes
-    values_at(* each_index.select(&:odd?))
+  def odd_values
+    e = [false, true].cycle
+    select { e.next }
+  end
+  def even_values
+    e = [true, false].cycle
+    select { e.next }
   end
 end
--- file_reading.rb
+++ file_reading.rb
@@ -1,20 +1,43 @@
 require 'csv'
 require_relative 'array_mods'
+require 'pry'
 
 def csv_to_array(filename)
     csv = CSV::parse(File.open('data.csv', 'r:ISO-8859-1') {|line| line.read })
     fields = csv.shift
     fields = fields.map {|f| f.downcase.gsub(" ", "_")}
-    movies_ratings_users = csv.collect { |record| Array[*fields.zip(record).flatten ] }
-    return movies_ratings_users
+    records = csv.collect { |record| Hash[*fields.zip(record).flatten ] }
+    return records
 end
 
 all_movies = csv_to_array('data.csv')
 
-all_movies[1] = all_movies.vget_odd_indexes
+movie_id_and_rating = []
+all_movies = all_movies[0..10].sort_by { |movie| movie[:movie_id] }.each do |movie|
+  movie_id_and_rating << [movie["movie_id"].to_i, movie["rating"].to_i]
+  # movie_id_and_rating <<
+end
+
+puts "#{movie_id_and_rating}"
+
+# movie_id_and_rating.select.each_with_index { |_, i| i.odd? }
+# if movie_id_and_rating.select.each_with_index { |_, i| i.even? } == 1
+#   puts "hi"
+# elsif movie_id_and_rating.select.each_with_index { |_, i| i.odd? } == 1
+#   puts "no"
+# # end
+
+tomato = gets.chomp.to_i
+if movie_id_and_rating.odd_values == tomato
+  puts "hi"
+end
 
 
-puts "#{all_movies[1]}"
+
+
+
+# csv = csv_row.fields.to_csv( options )
+# puts test_movies
 # ITS WORRRRRKINGGG!!!
 
 # new_hash = {}
