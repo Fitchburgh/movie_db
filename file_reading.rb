c6c46908e8c204526685099b660040b0693806e9
Brandon Fitch <fitchbj@gmail.com>
2016-09-23 21:35:46 -0400

    Have all ratings and avg ratings per movie calculating correctly via array.

--- file_reading.rb
+++ file_reading.rb
@@ -3,42 +3,44 @@ require_relative 'array_mods'
 require 'pry'
 
 def csv_to_array(filename)
-    csv = CSV::parse(File.open('data.csv', 'r:ISO-8859-1') {|line| line.read })
-    fields = csv.shift
-    fields = fields.map {|f| f.downcase.gsub(" ", "_")}
-    records = csv.collect { |record| Hash[*fields.zip(record).flatten ] }
-    return records
+  csv = CSV::parse(File.open('data.csv', 'r:ISO-8859-1') {|line| line.read })
+  fields = csv.shift
+  fields = fields.map {|f| f.downcase.gsub(" ", "_")}
+  records = csv.collect { |record| Hash[*fields.zip(record).flatten ] }
+  return records
 end
 
 all_movies = csv_to_array('data.csv')
 
-movie_id_and_rating = []
-all_movies = all_movies[0..10].sort_by { |movie| movie[:movie_id] }.each do |movie|
-  movie_id_and_rating << [movie["movie_id"].to_i, movie["rating"].to_i]
-  # movie_id_and_rating <<
+unsorted = []
+all_movies = all_movies.sort_by { |row| row[:movie_id] }.each do |row|
+  unsorted << [row["movie_id"].to_i, row["rating"].to_i]
 end
 
-puts "#{movie_id_and_rating}"
+# movie_id_and_rating = Hash[unsorted].each do |x|
+#   [unsorted[0], unsorted[1]]
+# end
 
-# movie_id_and_rating.select.each_with_index { |_, i| i.odd? }
-# if movie_id_and_rating.select.each_with_index { |_, i| i.even? } == 1
-#   puts "hi"
-# elsif movie_id_and_rating.select.each_with_index { |_, i| i.odd? } == 1
-#   puts "no"
-# # end
 
-tomato = gets.chomp.to_i
-if movie_id_and_rating.odd_values == tomato
-  puts "hi"
+
+
+
+movie_id = gets.chomp.to_i
+movie_ratings = []
+
+unsorted.each do |row|
+  if row[0] == movie_id
+    movie_ratings << row[1]
+  end
 end
 
+def ratings_avg(movie_ratings)
+  sum = 0
+  movie_ratings.each { |b| sum += b }
+  average_movie_rating = (sum.to_f / movie_ratings.count.to_f).round(1)
+end
 
+r = ratings_avg(movie_ratings)
+puts "#{r}"
 
-
-
-# csv = csv_row.fields.to_csv( options )
-# puts test_movies
-# ITS WORRRRRKINGGG!!!
-
-# new_hash = {}
-# all_movies.each { |i| new_hash[i] = hash.delete(i)}
+puts "#{movie_ratings}"
